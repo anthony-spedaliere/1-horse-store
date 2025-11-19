acffc377ee6bd53a483da10d6beefb7cf1d64e11
Anthony Spedaliere <avspedaliere@gmail.com>
2025-11-18 19:28:27 -0800

    - created huff test files

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+// import {HorseStoreYul} from "../../src/horseStoreV1/HorseStoreYul.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {    
+    HorseStore horseStore;
+
+    function setUp() public virtual {        
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 initialValue = horseStore.readNumberOfHorses();
+        assertEq(initialValue, 0);
+    }
+
+    function testWriteValue() public {
+        uint256 numberOfHorses = 777;
+        horseStore.updateHorseNumber(numberOfHorses);
+        assertEq(horseStore.readNumberOfHorses(), numberOfHorses);
+    }
+}
